a323188e4dcbf469b303e8df4daffa6d9c9e7034
HalaEzzat <hala.elhamahmy@gmail.com>
2022-04-11 13:49:18 +0200

    Creating Feeds

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
 
 
 sitemaps = {
@@ -12,4 +13,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+    path("feed/rss", LatestPostsFeed(), name="post_feed"),
 ]
